3635dc6c6cb778ec4234a1be5de1e7a0a72edadc
gusakovs <contact@maruku.be>
2023-11-22 11:14:18 +0100

    push beta jsp ce que j'ai fait

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import os
 import time
 from wand.image import Image
 from random_word import RandomWords
+from discord import app_commands
+
 
 def dl_image(atach):
     img_data = requests.get(atach.url).content
@@ -21,9 +23,15 @@ async def seam(message):
     name = dl_image(message.attachments[0])
     
     image = Image(filename='./convert/' + name + '.jpg')
+    
+    i = 1
 
     with image.clone() as liquid:
-        liquid.liquid_rescale(int(image.width*0.95), int(image.height*0.75))
+        liquid.liquid_rescale(int(image.width*0.65), int(image.height*0.45))
+        if i:
+            liquid.modulate(100,200,100)
+            liquid.brightness_contrast(0.0, 60)
+            liquid.adaptive_sharpen(10.2, 4.3)
         liquid.save(filename='./convert/' + name + '_crop.jpg')
 
     await message.channel.send(file=discord.File('./convert/' + name + '_crop.jpg'))
@@ -38,8 +46,6 @@ class MyClient(discord.Client):
         if(message.attachments):
             await seam(message)
 
-
-
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -47,11 +53,10 @@ with open('config.json') as f:
     data = json.load(f)
     token = data["token"]
 
-'''
-with open('ids.json') as f:
-    data = json.load(f)
-    maxime = data["maxime"]
-    memeid = data["memeid"]
-'''
 client = MyClient(intents=intents)
 client.run(token)
+tree = app_commands.CommandTree(client)
+
+@tree.command(name="coucou-mv")
+async def foo(ctx, arg):
+    ctx.send(arg)
